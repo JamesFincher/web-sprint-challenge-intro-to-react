29207cd7525e622a578a904068562c2b4aa4d29f
heartandkarma <heartandkarma@gmail.com>
2022-03-24 18:51:48 -0700

    got slices of state set, and axios pulling the data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [data, setData] = useState([]);
+  const [person, setCurrentPerson] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [nextPage, setNextPage] = useState('');
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people')
+      .then((res) => setData(res.data.results))
+      .then((res) => setNextPage(res.data.next))
+      .catch((err) => console.log(err));
+  }, []);
+  useEffect(() => {
+    console.table(data);
+  }, [data]);
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className='App'>
+      <h1 className='Header'>Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
